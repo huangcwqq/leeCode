8eccb58de6f1f8b0833d1e1c1fda0399c46c59a3
huangchengwei <229834707@qq.com>
2019-11-09 12:00:36 +0800

    旋转图片 两个数组的交集

--- src/main/java/Arrays/Intersect.java
+++ src/main/java/Arrays/Intersect.java
@@ -21,12 +21,130 @@
  */
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 public class Intersect {
     public static void main(String[] args) {
-
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+        int[] nums3 = {-2147483648,1,2,3};
+        int[] nums4 = {1,-2147483648,-2147483648};
+        int[] result = intersect3(nums3,nums4);
+        for(int i = 0;i<result.length;i++){
+            System.out.print(result[i]);
+            System.out.print(",");
+        }
     }
 
-    public int[] intersect(int[] nums1, int[] nums2) {
-        return null;
+    //垃圾方法
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> temp = new ArrayList();
+        List<Integer> num1List = new ArrayList();
+        List<Integer> num2List = new ArrayList();
+        for(int i=0;i<nums1.length;i++){
+            num1List.add(nums1[i]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            num2List.add(nums2[i]);
+        }
+        Iterator iterator1 = num1List.iterator();
+        while (iterator1.hasNext()){
+            Integer i = (Integer) iterator1.next();
+            Iterator iterator2 = num2List.iterator();
+            while (iterator2.hasNext()){
+                Integer j = (Integer)iterator2.next();
+                if(i.equals(j)){
+                    temp.add(j);
+                    iterator1.remove();
+                    iterator2.remove();
+                    break;
+                }
+            }
+        }
+        int[] result = new int[temp.size()];
+        for(int i=0;i<temp.size();i++){
+            result[i] = temp.get(i);
+        }
+        return result;
     }
+
+    //对方法1进行优化
+    public static int[] intersect2(int[] nums1, int[] nums2) {
+        List<Integer> temp = new ArrayList();
+        List<Integer> num1List = new ArrayList();
+        List<Integer> num2List = new ArrayList();
+        for(int i=0;i<nums1.length;i++){
+            num1List.add(nums1[i]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            num2List.add(nums2[i]);
+        }
+        for(Integer num : num2List){
+            if(num1List.contains(num)){
+                temp.add(num);
+                num1List.remove(num);
+            }
+        }
+        int[] result = new int[temp.size()];
+        for(int i=0;i<temp.size();i++){
+            result[i] = temp.get(i);
+        }
+        return result;
+    }
+
+    //引入map进行优化
+    public static int[] intersect3(int[] nums1, int[] nums2) {
+        List<Integer> temp = new ArrayList();
+        Map<Integer,Integer> tempMap = new HashMap<Integer, Integer>();
+        for(int i=0;i<nums1.length;i++){
+            Integer value = tempMap.get(nums1[i]);
+            if(value == null){
+                tempMap.put(nums1[i],1);
+            }else {
+                tempMap.put(nums1[i],value + 1);
+            }
+        }
+        for(int j = 0;j < nums2.length;j++){
+            Integer value = tempMap.get(nums2[j]);
+            if(value != null && value >= 1){
+                temp.add(nums2[j]);
+                tempMap.put(nums2[j],value - 1);
+            }
+        }
+        int[] result = new int[temp.size()];
+        for(int i=0;i<temp.size();i++){
+            result[i] = temp.get(i);
+        }
+        return result;
+    }
+
+    //对两个数组先进行排序
+    public static int[] intersect4(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> temp = new ArrayList();
+        int p1 = 0, p2 = 0;
+        while(p1 < nums1.length && p2 < nums2.length){
+            if(nums2[p2] == nums1[p1]){
+                temp.add(nums1[p1]);
+                p1 ++;
+                p2 ++;
+            }else if(nums1[p1] < nums2[p2]){
+                p1 ++;
+            }else if(nums1[p1] > nums2[p2]){
+                p2++;
+            }
+        }
+        int[] res = new int[temp.size()];
+        for(int i = 0; i < temp.size(); i ++){
+            res[i] = temp.get(i);
+        }
+        return res;
+    }
+
 }
--- src/main/java/Arrays/RotateImage.java
+++ src/main/java/Arrays/RotateImage.java
@@ -43,4 +43,72 @@
 package Arrays;
 
 public class RotateImage {
+    public static void main(String[] args) {
+        int[][] matrix  =
+                {
+                        {1,2,3},
+                        {4,5,6},
+                        {7,8,9},
+                };
+//        int[][] matrix2  =
+//                {
+//                        {1,2,3,4},
+//                        {5,6,7,8},
+//                        {9,10,11,12},
+//                        {13,14,15,16},
+//                };
+        int[][] matrix2  =
+                {
+                        {1,2},
+                        {3,4},
+                };
+
+        rotate2(matrix2);
+        for(int i = 0;i < matrix2.length;i++){
+            for(int j = 0;j < matrix2[i].length;j++){
+                System.out.print(matrix2[i][j]);
+                System.out.print(",");
+            }
+            System.out.println();
+        }
+    }
+
+    //使用了另外一个矩阵辅助，不合题意
+    public static void rotate(int[][] matrix) {
+        if(matrix.length < 2){
+            return;
+        }
+        int[][] result = new int[matrix.length][matrix.length];
+        int columnLength = matrix.length - 1;
+        for(int i = 0;i < matrix.length;i++){
+            for(int j = 0;j < matrix[i].length;j++){
+                result[j][columnLength] = matrix[i][j];
+            }
+            columnLength--;
+        }
+        for(int i = 0;i < result.length;i++){
+            for(int j = 0;j < result[i].length;j++){
+                matrix[i][j] = result[i][j];
+            }
+        }
+    }
+
+    //找出规律，再用规律解题
+    public static void rotate2(int[][] matrix) {
+        int len = matrix.length - 1;
+        int length = len;
+        if(len < 1){
+            return;
+        }
+        for(int i = 0;i < length ;i++){
+            for(int j = i;j < length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len - j][i];
+                matrix[len - j][i] = matrix[len - i][len - j];
+                matrix[len - i][len - j] = matrix[j][len - i];
+                matrix[j][len - i] = temp;
+            }
+            length = length - 1;
+        }
+    }
 }
